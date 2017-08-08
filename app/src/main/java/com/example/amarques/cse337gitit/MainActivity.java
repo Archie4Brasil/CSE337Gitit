73a26a90a6458173a74cc8de691f0b4e92329af3
amarques <amarques@oakland.edi>
2017-08-08 19:42:45 -0400

    init commit

--- app/src/main/java/com/example/amarques/cse337gitit/MainActivity.java
+++ app/src/main/java/com/example/amarques/cse337gitit/MainActivity.java
@@ -9,10 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // but I don't like this
-        // I am still alive
-        // end of testing branch
-        // so is testing really dead?
-        // making it official
+
     }
 }
