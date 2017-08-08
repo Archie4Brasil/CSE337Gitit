374a792d6a0bffe339458670b0a270fe3da1f279
amarques <amarques@oakland.edi>
2017-08-08 19:36:01 -0400

    end of testing branch??

--- app/src/main/java/com/example/amarques/cse337gitit/MainActivity.java
+++ app/src/main/java/com/example/amarques/cse337gitit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // but I don't like this
+        // end of testing branch
     }
 }
